33bf7b173e058ed703de640e1b1fa9a78bf0697d
13dashaa <168581575+13dashaa@users.noreply.github.com>
2024-05-11 22:17:58 +0300

    Update solution.cpp

--- УПОзн/solution.cpp
+++ УПОзн/solution.cpp
@@ -5,13 +5,13 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	result answer_character[2] = { {0,"интроверт"}, {0, "экстраверт"} };
-	result temperament_results[4] = { {0,"сангвиник"}, {0,"холерик"}, {0,"флегматик"}, {0,"меланхолик"} };
+	result answer_character[2] = { {0,"introvert"}, {0, "ekstravert"} };
+	result temperament_results[4] = { {0,"sangvinic"}, {0,"holeric"}, {0,"flegmatic"}, {0,"melanholic"} };
 	int flag;
 	do
 	{
 		int amount;
-		printf("Введите количество людей, которые будут проходить тест\n");
+		printf("enter amount of people\n");
 		enter_int(&amount);
 		Node* head = NULL;
 		system("CLS");
@@ -20,9 +20,9 @@ int main()
 			rewind(stdin);
 			person current_person;
 			FILE* f = NULL;
-			printf("Введите никнейм %d% человека.\n", i + 1);
+			printf("Р’РІРµРґРёС‚Рµ РЅРёРєРЅРµР№Рј %d% С‡РµР»РѕРІРµРєР°.\n", i + 1);
 			enter_str(current_person.nickname, N, stdin);
-			printf("Если хотите определить тип темперамента, введите 1. Если же хотите проити тест на характер, введите 2.\n");
+			printf("Р•СЃР»Рё С…РѕС‚РёС‚Рµ РѕРїСЂРµРґРµР»РёС‚СЊ С‚РёРї С‚РµРјРїРµСЂР°РјРµРЅС‚Р°, РІРІРµРґРёС‚Рµ 1. Р•СЃР»Рё Р¶Рµ С…РѕС‚РёС‚Рµ РїСЂРѕРёС‚Рё С‚РµСЃС‚ РЅР° С…Р°СЂР°РєС‚РµСЂ, РІРІРµРґРёС‚Рµ 2.\n");
 			enter_answer_charct(&current_person.type_test);
 			system("CLS");
 			int size;
@@ -41,33 +41,33 @@ int main()
 				test_result = answer_character;
 			}
 			test(f, file_name, size, &current_person, test_result);
-			add_el(&head, current_person);													//добавление структуры человека в список. Список сортируется по алфавиту
+			add_el(&head, current_person);													//Г¤Г®ГЎГ ГўГ«ГҐГ­ГЁГҐ Г±ГІГ°ГіГЄГІГіГ°Г» Г·ГҐГ«Г®ГўГҐГЄГ  Гў Г±ГЇГЁГ±Г®ГЄ. Г‘ГЇГЁГ±Г®ГЄ Г±Г®Г°ГІГЁГ°ГіГҐГІГ±Гї ГЇГ® Г Г«ГґГ ГўГЁГІГі
 			person current_person_copy = create_copy(current_person, current_person.type_test);
 			sort_result(&current_person_copy, current_person.type_test);
-			printf("%s%s%s\nРезультат теста не является достоверным на 100 %% и имеет исключительно информативный характер.\nНажмите для продолжения\n", RED, "ВНИМАНИЕ!!!", RESET);
+			printf("%s%s%s\nР РµР·СѓР»СЊС‚Р°С‚ С‚РµСЃС‚Р° РЅРµ СЏРІР»СЏРµС‚СЃСЏ РґРѕСЃС‚РѕРІРµСЂРЅС‹Рј РЅР° 100 %% Рё РёРјРµРµС‚ РёСЃРєР»СЋС‡РёС‚РµР»СЊРЅРѕ РёРЅС„РѕСЂРјР°С‚РёРІРЅС‹Р№ С…Р°СЂР°РєС‚РµСЂ.\nРќР°Р¶РјРёС‚Рµ РґР»СЏ РїСЂРѕРґРѕР»Р¶РµРЅРёСЏ\n", RED, "Р’РќРРњРђРќРР•!!!", RESET);
 			_getch();
 			system("CLS");
-			print_result(current_person_copy);												//вывод результатов тестирования на экран				
-			printf("\nЕсли хотите узнать расшифровку теста введите 1.\n");
+			print_result(current_person_copy);												//ГўГ»ГўГ®Г¤ Г°ГҐГ§ГіГ«ГјГІГ ГІГ®Гў ГІГҐГ±ГІГЁГ°Г®ГўГ Г­ГЁГї Г­Г  ГЅГЄГ°Г Г­				
+			printf("\nР•СЃР»Рё С…РѕС‚РёС‚Рµ СѓР·РЅР°С‚СЊ СЂР°СЃС€РёС„СЂРѕРІРєСѓ С‚РµСЃС‚Р° РІРІРµРґРёС‚Рµ 1.\n");
 			int sign;
 			enter_int(&sign);
 			if (sign == 1)
 			{
-				print_information(current_person.type_test, current_person_copy);			//вывод пояснения результата
+				print_information(current_person.type_test, current_person_copy);			//ГўГ»ГўГ®Г¤ ГЇГ®ГїГ±Г­ГҐГ­ГЁГї Г°ГҐГ§ГіГ«ГјГІГ ГІГ 
 				_getch();
 			}
 			system("CLS");
 			free(current_person_copy.results);
 		}
 		FILE* f_res = NULL;
-		file_write(f_res, "result", head);												  //запись в файл
-		printf("\nЕсли хотите выполнить программу еще раз, введите 1. Иначе, введите 0.\n");					//запрос на повторное выполнение програмы
+		file_write(f_res, "result", head);												  //Г§Г ГЇГЁГ±Гј Гў ГґГ Г©Г«
+		printf("\nР•СЃР»Рё С…РѕС‚РёС‚Рµ РІС‹РїРѕР»РЅРёС‚СЊ РїСЂРѕРіСЂР°РјРјСѓ РµС‰Рµ СЂР°Р·, РІРІРµРґРёС‚Рµ 1. РРЅР°С‡Рµ, РІРІРµРґРёС‚Рµ 0.\n");					//Г§Г ГЇГ°Г®Г± Г­Г  ГЇГ®ГўГІГ®Г°Г­Г®ГҐ ГўГ»ГЇГ®Г«Г­ГҐГ­ГЁГҐ ГЇГ°Г®ГЈГ°Г Г¬Г»
 		int val2;
 		do {
 			rewind(stdin);
-			val2 = scanf_s("%d", &flag);																		//проверка ввода
+			val2 = scanf_s("%d", &flag);																		//ГЇГ°Г®ГўГҐГ°ГЄГ  ГўГўГ®Г¤Г 
 			if (!val2 || (flag != 0 && flag != 1))
-				printf("Неверный ввод. Повторите попытку.\n");
+				printf("erorr.\n");
 		} while (!val2 || (flag != 0 && flag != 1));
 		system("CLS");
 		clear_ring(&head);
